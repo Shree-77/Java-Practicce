adef695f7e483265191a7d0445591033dd76789e
Shree <109030986+Shree-77@users.noreply.github.com>
2023-04-16 19:13:56 +0530

    4th question updated

---
+++ JavaPractice/src/question4/Main.java
@@ -0,0 +1,21 @@
+package question4;
+
+public class Main {
+    public static void main(String[] args) {
+        // Vector addition and multiplication
+        Vector v1 = new Vector(new int[]{1, 2, 3});
+        Vector v2 = new Vector(new int[]{4, 5, 6});
+        v1.add(v2);
+        System.out.println("Vector addition: " + v1);
+        v1.multiply(v2);
+        System.out.println("Vector multiplication: " + v1);
+
+        // Matrix addition and multiplication
+        Matrix m1 = new Matrix(new int[][]{{1, 2}, {3, 4}});
+        Matrix m2 = new Matrix(new int[][]{{5, 6}, {7, 8}});
+        m1.add(m2);
+        System.out.println("Matrix addition: " + m1);
+        m1.multiply(m2);
+        System.out.println("Matrix multiplication: " + m1);
+    }
+}
---
+++ JavaPractice/src/question4/Matrix.java
@@ -0,0 +1,46 @@
+package question4;
+
+import java.util.Arrays;
+
+
+        public  class Matrix implements Operations {
+        private int[][] matrix;
+
+        public Matrix(int[][] matrix) {
+            this.matrix = matrix;
+        }
+
+        public void add(Operations other) {
+            question4.Matrix m = (question4.Matrix) other;
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    matrix[i][j] += m.matrix[i][j];
+                }
+            }
+        }
+
+        public void multiply(Operations other) {
+            question4.Matrix m = (question4.Matrix) other;
+            int[][] result = new int[matrix.length][m.matrix[0].length];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < m.matrix[0].length; j++) {
+                    int sum = 0;
+                    for (int k = 0; k < matrix[0].length; k++) {
+                        sum += matrix[i][k] * m.matrix[k][j];
+                    }
+                    result[i][j] = sum;
+                }
+            }
+            matrix = result;
+        }
+
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (int[] row : matrix) {
+                sb.append(Arrays.toString(row)).append("\n");
+            }
+            return sb.toString();
+        }
+    }
+
+
