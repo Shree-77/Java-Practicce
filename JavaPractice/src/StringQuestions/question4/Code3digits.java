c32fbd01b1bc65dcb0a427407f74f850b12d3855
Shree <109030986+Shree-77@users.noreply.github.com>
2023-04-22 13:58:11 +0530

    Question 4,5 Solved

--- JavaPractice/src/StringQuestions/question4/Code3digits.java
+++ JavaPractice/src/StringQuestions/question4/Code3digits.java
@@ -1,2 +1,19 @@
-package StringQuestions.question4;public class Code3digits {
+package StringQuestions.question4;
+
+import java.util.Scanner;
+
+public class Code3digits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer code for a character (0-255): ");
+        int code = scanner.nextInt();
+        char c = (char) code;
+        System.out.println("The corresponding character is: " + c);
+        System.out.println("All possible three-digit codes and corresponding characters:");
+        for (int i = 0; i <= 255; i++) {
+            String codeStr = String.format("%03d", i);
+            char ch = (char) i;
+            System.out.println(codeStr + " -> " + ch);
+        }
+    }
 }
